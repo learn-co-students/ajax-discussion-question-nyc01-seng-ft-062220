9a8c02c6ff99749b2c28cf632895e68e7571af92
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-07 11:03:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
-  document.addEventListener("click", function(e){
-
-
+  document.addEventListener("click", e => {
     if (e.target.matches("button.btn.btn-primary")) {
       fetchData()
     }
-  })
+  });
 
-  function fetchData(){
-    let data = fetch('https://randomuser.me/api/')
-      .then( res => res.json() )
-      .then( res => processData(res) )
+  const fetchData = () => {
+    fetch('https://randomuser.me/api/')
+      .then( res => res.json())
+      .then( data => {
+        const user = data.results[0]
+        processData(user)
+      })
+  };
 
-  }
+  const processData = (user) => {
 
-function processData(res) {
-    let object = {
-      "name" : res.name,
+    const picture = user.picture.large
 
-    }
+    document.querySelector('#profile_picture').src = picture
 
-}
+    const first = user.name.first
+    const last = user.name.last
+    const title = user.name.title
+
+    document.querySelector('#fullname').textContent = `${title} ${first} ${last}`
+
+};
+
+
 
 
 });
